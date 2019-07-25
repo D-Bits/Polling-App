9ddea7517258caa808f76172c786aeae50ba954d
root <root@8af67c771989>
2019-07-25 21:07:34 +0000

    Configured to use Postgres inside container.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'polling',
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('PG_PASS'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
